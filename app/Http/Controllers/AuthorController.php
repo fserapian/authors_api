bca71a7ec85b0a348c2e3255765fcc04291f5f05
Fadi Serapian <fadi.serapian@gmail.com>
2020-03-13 19:15:20 -0400

    first commit

---
+++ app/Http/Controllers/AuthorController.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Author;
+use App\Traits\ApiResponsor;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
+class AuthorController extends Controller
+{
+    use ApiResponsor;
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     *  Return the list of authors
+     *  @return Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $authors =  Author::all();
+        return $this->successResponse($authors);
+    }
+
+    /**
+     *  Create a new author
+     *  @return Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $rules = [
+            'name' => 'required|max:255',
+            'gender' => 'required|max:255|in:male,female',
+            'country' => 'required|max:255'
+        ];
+
+        $this->validate($request, $rules);
+
+        $author = Author::create($request->all());
+
+        return $this->successResponse($author, Response::HTTP_CREATED);
+    }
+
+    /**
+     *  Obtain and show one author
+     *  @return Illuminate\Http\Response
+     */
+    public function show($author)
+    {
+    }
+
+    /**
+     *  Update an existing author
+     *  @return Illuminate\Http\Response
+     */
+    public function update(Request $request,  $author)
+    {
+    }
+
+    /**
+     *  Remove an existing author
+     *  @return Illuminate\Http\Response
+     */
+    public function destroy($author)
+    {
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,19 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It is a breeze. Simply tell Lumen the URIs it should respond to
+| and give it the Closure to call when that URI is requested.
+|
+*/
+
+//$router->get('/', function () use ($router) {
+//    return 'Welcome to your first lumen app';
+//});
+
+$router->get('/authors', 'AuthorController@index');
+$router->post('/authors', 'AuthorController@store');
